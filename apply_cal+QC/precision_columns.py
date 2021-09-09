2791d284802174737c523a898117f001b9683b75
Dean Henze <deanchenze@gmail.com>
2021-09-09 16:46:02 -0700

    Added a script to compute precision columns. Changed run_fullcal.py to include precision columns script. Added data file header template .txt files.

---
+++ apply_cal+QC/precision_columns.py
@@ -0,0 +1,67 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep  7 17:20:24 2021
+
+@author: DeanH808
+
+Single function in this script is called to add precision columns to a single 
+WISPER data file.
+"""
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def add_precision_cols(data, date, test_plot=False):
+    """
+    data: pandas.DataFrame:
+        Dataframe to add precision columns to.
+        
+    date: str.
+        Date of ORACLES flight, 'yyyymmdd'.
+        
+    test_plot: bool.
+        Set to True to make a test plot of computed precisions.
+        
+    Returns:
+        None, 'data' is modified in place.
+    """
+    
+    # Calculate Pi2 precisions as functions of log humidity:
+    data['std_dD_tot2'] = 6*10**6*np.log(data['h2o_tot2'])**-6.69
+    data['std_d18O_tot2'] = 6170*np.log(data['h2o_tot2'])**-4.72
+        
+        
+    # For 2017 and 2018, also make precision columns for Pic1:  
+    if date[:4] in ['2017','2018']:
+        pic1_h2okeys = ['h2o_tot1','h2o_cld']
+        std_D_pic1 = 9*10**6*np.log(data[pic1_h2okeys])**-6.66 
+        std_18O_pic1 = 451000*np.log(data[pic1_h2okeys])**-6.56 
+    # Assign precision data to columns in 'data':
+        data['std_dD_tot1']=std_D_pic1['h2o_tot1']
+        data['std_dD_cld']=std_D_pic1['h2o_cld']
+        data['std_d18O_tot1']=std_18O_pic1['h2o_tot1']
+        data['std_d18O_cld']=std_18O_pic1['h2o_cld']
+
+
+    # Optional test plot; plot data std's vs humidity:
+    if test_plot:
+        plt.figure('Precisions test')
+        ax1=plt.subplot(1,2,1)   
+        ax1.plot(np.log(data['h2o_tot2']), data['std_dD_tot2'], 'ro')    
+        ax2=plt.subplot(1,2,2)   
+        ax2.plot(np.log(data['h2o_tot2']), data['std_d18O_tot2'], 'ro')  
+        if date[:4] in ['2017','2018']:
+            ax1.plot(np.log(data['h2o_tot1']), data['std_dD_tot1'], 'bo')    
+            ax1.plot(np.log(data['h2o_cld']), data['std_dD_cld'], 'bo') 
+            ax2.plot(np.log(data['h2o_tot1']), data['std_d18O_tot1'], 'bo')    
+            ax2.plot(np.log(data['h2o_cld']), data['std_d18O_cld'], 'bo')                 
+                
+        # Plot labels:
+    if test_plot:
+        plt.figure('Precisions test', fontsize=14)
+        ax1.set_title('ORACLES WISPER precision test plot', fontsize=14)
+        ax1.set_xlabel('log humidity, log(ppmv)', fontsize=14)
+        ax1.set_ylabel('std dD, permil', fontsize=14)
+        ax2.set_ylabel('std d18O, permil', fontsize=14)
--- apply_cal+QC/run_fullcal.py
+++ apply_cal+QC/run_fullcal.py
@@ -9,12 +9,16 @@ data files.
 """
 
 
+# Built in:
+import os
+
 # Third party:
 import numpy as np
 
 # My modules:
 import preprocess 
 import time_sync
+import precision_columns as stdcols
 import pic1_cal
 import pic2_cal
 
@@ -22,6 +26,7 @@ import pic2_cal
 # Paths to raw and (to-be) calibrated file directories:
 path_rawdir = r"./WISPER_raw_data/"
 path_caldir = r"./WISPER_calibrated_data/"
+if not os.path.exists(path_caldir): os.makedirs(path_caldir)
 
 
 # ORACLES research flight dates where WISPER took good data:
@@ -36,11 +41,13 @@ dates2018_good = ['20180927','20180930','20181003','20181007','20181010',
                   '20181023']
 
 
+
 # Calibration of a single file:
 def calibrate_file(date):
     """
-    Calibrate a WISPER file for the P3 flight on the passed date ('yyyymmdd').
+    Calibrate a WISPER file for the P3 flight on the input date ('yyyymmdd').
     """
+    year = date[:4]
     
     # Preprocessing procedure:
     pre = preprocess.Preprocessor(date)
@@ -54,10 +61,13 @@ def calibrate_file(date):
     data_syncd = time_sync.wisper_tsync(data_prepro, date)
     #time_sync.test_plot(pre.preprodata, data_syncd, date)
     
+    # Add precision columns:
+    stdcols.add_precision_cols(data_syncd, date, test_plot=False)
+    
     # Pic1 calibration (only relevant for 2017 and 2018 sampling periods):
-    if date[:4] in ['2017','2018']:
-        #data_pic1cal = pic1_cal.apply_cal(data_syncd, date, testplots=False)
-        data_pic1cal = pic1_cal.apply_cal(data_syncd, date, testplots=True)
+    if year in ['2017','2018']:
+        data_pic1cal = pic1_cal.apply_cal(data_syncd, date, testplots=False)
+        #data_pic1cal = pic1_cal.apply_cal(data_syncd, date, testplots=True)
     else: 
         data_pic1cal = data_syncd
         
@@ -65,17 +75,87 @@ def calibrate_file(date):
     data_pic2cal = pic2_cal.apply_cal(data_pic1cal, date, testplots=False)
     #data_pic2cal = pic2_cal.apply_cal(data_pic1cal, date, testplots=True)
     
-    # Save calibrated data:
+    # Decimal rounding:
+    if year == '2016':
+        data_pic2cal = data_pic2cal.round({'Start_UTC':0, 'h2o_tot2':0, 
+                                           'dD_tot2':1, 'std_dD_tot2':1, 
+                                           'd18O_tot2':1, 'std_d18O_tot2':1})
+    if year in ['2017','2018']:
+        data_pic2cal = data_pic2cal.round({'Start_UTC':0, 'wisper_valve_state':0, 
+                                           'h2o_tot1':0, 'h2o_tot2':0, 
+                                           'h2o_cld':0, 'dD_tot1':1, 
+                                           'std_dD_tot1':1, 
+                                           'dD_tot2':1, 'std_dD_tot2':1, 
+                                           'dD_cld':1, 'std_dD_cld':1, 
+                                           'd18O_tot1':1, 'std_d18O_tot1':1,
+                                           'd18O_tot2':1, 'std_d18O_tot2':1, 
+                                           'd18O_cld':1, 'std_d18O_cld':1,
+                                           'cvi_lwc':2, 'cvi_enhance':2, 
+                                           'cvi_dcut50':2, 'cvi_inFlow':2,
+                                           'cvi_xsFlow':2, 'cvi_userFlow':2})
+        
+    # Rearrange columns:
+    if year=='2016':
+        data_pic2cal = data_pic2cal[['Start_UTC','h2o_tot2', 'dD_tot2', 
+                                     'std_dD_tot2','d18O_tot2', 'std_d18O_tot2']]
+    if year in ['2017','2018']:
+        data_pic2cal = data_pic2cal[['Start_UTC', 'wisper_valve_state', 'h2o_tot1', 
+                                     'h2o_tot2', 'h2o_cld', 'dD_tot1', 
+                                     'std_dD_tot1', 'dD_tot2', 'std_dD_tot2', 
+                                     'dD_cld', 'std_dD_cld', 'd18O_tot1', 
+                                     'std_d18O_tot1', 'd18O_tot2', 'std_d18O_tot2', 
+                                     'd18O_cld', 'std_d18O_cld', 'cvi_lwc', 
+                                     'cvi_enhance', 'cvi_dcut50', 'cvi_inFlow', 
+                                     'cvi_xsFlow', 'cvi_userFlow']]
+    
+    # Save calibrated data in a temporary file:
     data_pic2cal.fillna(-9999, inplace=True)  
     fname = "WISPER_calibrated_%s.ict" % date
     data_pic2cal.to_csv(path_caldir + fname, index=False)
     
-    del pre, data_syncd, data_pic1cal
+    # Add file header and save with final filename:
+    add_file_header(date)
+    
+    del pre, data_syncd, data_pic1cal, data_pic2cal
+    
+    
+    
+# Add file header:
+def add_file_header(date):
+    """
+    Add a file header to a calibrated WISPER file.
+    """
+    
+    # Get header text as list of strings from file:
+    year = date[:4]
+    fname_header = r'./ORACLES_WISPER_file_header_template_'+year+'.txt'  
+    with open(fname_header, mode='r') as file_header:
+        header = file_header.readlines() # read header text
+    
+    # Modify the "date of collection/revision" line in the header text:
+        revDate = '20190901' # Latest revision date.
+        header[6] = date[0:4]+', '+date[4:6]+', '+date[6:8]+', '                  \
+                    +revDate[0:4]+', '+revDate[4:6]+', '+revDate[6:8]+'\n'
+    
+    # Open WISPER file and append header text:
+        fname_calfile = path_caldir + "WISPER_calibrated_%s.ict" % date
+        with open(fname_calfile, mode='r') as wisper:
+            text_wisper = wisper.read()
+        writetext = header+[text_wisper] # text to write to new file.
+    
+    # Write new file:
+        revNum = 'R2' # Revision number on ESPO site.
+        fname_final = path_caldir + 'WISPER_P3_'+date+'_'+revNum+'.ict'
+        with open(fname_final, mode='w') as file_w:
+            file_w.writelines(writetext)
+    
+    # Delete old file:
+        os.unlink(fname_calfile)
     
 
-#for date in dates2016_good[:3]: calibrate_file(date)
-for date in dates2017_good[4:7]: calibrate_file(date)
-#for date in dates2018_good: calibrate_file(date)
+for date in dates2016_good: calibrate_file(date)
+for date in dates2017_good: calibrate_file(date)
+for date in dates2018_good: calibrate_file(date)
 
 
 
