4f14480a1a70f5ba777361bd6e3bf6ed48c0b564
Dean Henze <deanchenze@gmail.com>
2022-05-07 13:54:04 -0700

    Handle missing values correctly.

--- level3_products/getdata.py
+++ level3_products/getdata.py
@@ -10,8 +10,11 @@ function 'data_singledate'
 """
 
 
+# Built in
+from warnings import filterwarnings
 
 # Third party
+import numpy as np
 import pandas as pd
 import netCDF4 as nc # 1.3.1
 
@@ -34,7 +37,31 @@ def wisperaddvars(date):
     ------
     date: str.
         Flight date, 'yyyymmdd'.
+        
+    Returns
+    -------
+    wisper_addvars: pandas.DataFrame
+        Missing values filled with np.nan.
+        Following vars + keys:
+            'Start_UTC': time in seconds
+            'lon', 'lat', 'height_m': latitude, longitude, height ASL in meters.
+            'h2o_gkg': total water mixing ratio.
+            'dD_permil', 'd18O_permil': total water isotope ratios.
+            'T_K', 'P_hPa': static air temperature and pressure.
+        
+            if 2017 or 2018 sampling period:
+            'cwc': cloud water content.
+            'dD_cld', 'd18O_cld': cloud water isotope ratios in permil.
+            'cvi_enhance': CVI enhancement factor.
     """
+
+    # Suppress two warnings triggered in this fxn:
+    filterwarnings(     # Harmless
+        action='ignore', category=DeprecationWarning, 
+        message='`np.bool` is a deprecated alias'
+        )
+    pd.options.mode.chained_assignment = None  # Falsely raised warning.
+
     
     year = date[0:4]
     
@@ -53,6 +80,7 @@ def wisperaddvars(date):
     merged_nc = nc.Dataset(
         relpath_merged + "mrg1_P3_%s_%s.nc" % tuple([date, merged_revnum])
         )
+
     if year in ['2016','2017']: altitude_key='MSL_GPS_Altitude'
     if year == '2018': altitude_key='GPS_Altitude'
     addvarkeys_nc = [altitude_key, 'Latitude', 'Longitude', 
@@ -63,6 +91,7 @@ def wisperaddvars(date):
     merged_pd['Start_UTC'] = merged_nc.variables['Start_UTC'][:]
     for knc, knew in zip(addvarkeys_nc, varkeys_assign):
         merged_pd[knew] = merged_nc.variables[knc][:]
+    merged_pd.replace(-9999, np.nan, inplace=True) # Change missing value flag.    
         
         # Convert temperature to units of degK:
     merged_pd['T_K'] = merged_pd['T_C'] + 273
@@ -73,15 +102,16 @@ def wisperaddvars(date):
 
 
     # Replace cloud h2o var with enhancement-corrected cloud water 
-    # content in units of g/m3:
-    wisper_addvars['cwc'] = cvi_cwc.cvi_cwc(
-        wisper_addvars['h2o_cld_gkg'].values, 
-        wisper_addvars['T_K'].values, 
-        wisper_addvars['P_hPa'].values*100, 
-        wisper_addvars['cvi_enhance'].values)
-    wisper_addvars.drop(labels='h2o_cld_gkg', axis=1, inplace=True)
+    # content in units of g/m3 (applicable for 2017 and 2018):
+    if year in ['2017','2018']:
+        wisper_addvars['cwc'] = cvi_cwc.cvi_cwc(
+            wisper_addvars['h2o_cld_gkg'].values, 
+            wisper_addvars['T_K'].values, 
+            wisper_addvars['P_hPa'].values*100, 
+            wisper_addvars['cvi_enhance'].values)
+        wisper_addvars.drop(labels='h2o_cld_gkg', axis=1, inplace=True)
         
-    
+        
     return wisper_addvars
 
 
@@ -108,6 +138,8 @@ def wisperdata(date):
         relpath_wisper + "WISPER_P3_%s_R2.ict" % date, 
         header=wisper_headerline
         )
+    wisper.replace(-9999, np.nan, inplace=True) # Change missing value flag.    
+
 
     if year == '2016': # Only Pic2 data available.
         wisper_new = wisper[['Start_UTC','h2o_tot2','dD_tot2','d18O_tot2']]
@@ -130,12 +162,11 @@ def wisperdata(date):
     if year in ['2017','2018']:
         for cloudkey in ['h2o_cld','dD_cld','d18O_cld','cvi_enhance']:
             wisper_new[cloudkey] = wisper[cloudkey]
-            
-            
-    # Convert cloud water from ppmv units to g/kg.
-    wisper_new['h2o_cld_gkg'] = convertq.ppmv_to_gkg(wisper_new['h2o_cld'])
-    wisper_new.drop(labels='h2o_cld', axis=1, inplace=True)
-    
+                    
+        # Convert cloud water from ppmv units to g/kg.
+        wisper_new['h2o_cld_gkg'] = convertq.ppmv_to_gkg(wisper_new['h2o_cld'])
+        wisper_new.drop(labels='h2o_cld', axis=1, inplace=True)
+        
 
     return wisper_new
 
