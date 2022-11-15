8ac8e365d02715944078ddd912259e710207e083
Dean Henze <deanchenze@gmail.com>
2022-11-15 09:17:17 -0800

    Removed some commented code. Added more documentation.

--- apply_cal+QC/run_fullcal.py
+++ apply_cal+QC/run_fullcal.py
@@ -4,9 +4,15 @@ Created on Thu Aug  5 15:28:54 2021
 
 @author: Dean
 
-Once complete, will run the full preprocessing and calibration of all WISPER 
-data files.
+Full preprocessing and calibration of all WISPER data files from a call to main.
+
+This script serves as the top-level interface for the user. It includes path 
+specification to raw data files and location to save processed files. It also 
+includes the flight dates for ORACLES files to process. For data processing, 
+it is primarily a wrapper for other scripts, although it does some tidying 
+of the data and applies a file header before saving. 
 """
+
 
 
 # Built in:
@@ -23,10 +29,24 @@ import pic1_cal
 import pic2_cal
 
 
+
 # Paths to raw and calibrated file directories:
 path_rawdir = r"./WISPER_raw_data/"
 path_caldir = r"./WISPER_calibrated_data/"
 if not os.path.exists(path_caldir): os.makedirs(path_caldir)
+
+
+
+# ORACLES research flight dates where WISPER took good data:
+dates2016_good = ['20160830','20160831','20160902','20160904','20160910',
+                  '20160912','20160914','20160918','20160920','20160924',
+                  '20160925']
+dates2017_good = ['20170812','20170813','20170815','20170817','20170818',
+                  '20170821','20170824','20170826','20170828','20170830',
+                  '20170831','20170902']
+dates2018_good = ['20180927','20180930','20181003','20181007','20181010',
+                  '20181012','20181015','20181017','20181019','20181021',
+                  '20181023']
 
 
 
@@ -40,13 +60,11 @@ def calibrate_file(date):
     # Preprocessing:
     pre = preprocess.Preprocessor(date) # Class instance loads the data.
     pre.preprocess_file() # Apply preprocessing.
-    #pre.test_plots()
     data_prepro = pre.preprodata.copy() # Copy to pass on for calibration.
     data_prepro.replace(-9999, np.nan, inplace=True)
 
     # Time synchronization:
     data_cal = time_sync.wisper_tsync(data_prepro, date)
-    #time_sync.test_plot(pre.preprodata, data_syncd, date)
     
     # Add precision columns:
     data_cal = stdcols.add_precision_cols(data_cal, date, test_plot=False)
@@ -54,13 +72,11 @@ def calibrate_file(date):
     # Pic1 calibration (only relevant for 2017 and 2018 sampling periods):
     if year in ['2017','2018']:
         data_cal = pic1_cal.apply_cal(data_cal, date, testplots=False)
-        #data_cal = pic1_cal.apply_cal(data_cal, date, testplots=True)
     else: 
         data_cal = data_cal
         
     # Pic2 calibration:
     data_cal = pic2_cal.apply_cal(data_cal, date, testplots=False)
-    #data_cal = pic2_cal.apply_cal(data_cal, date, testplots=True)
     
     # Decimal rounding:
     if year == '2016':
@@ -137,33 +153,17 @@ def add_file_header(date):
             file_w.writelines(writetext)
     
     # Delete old file:
-        os.unlink(fname_calfile)
-    
-
-
-# ORACLES research flight dates where WISPER took good data:
-dates2016_good = ['20160830','20160831','20160902','20160904','20160910',
-                  '20160912','20160914','20160918','20160920','20160924',
-                  '20160925']
-dates2017_good = ['20170812','20170813','20170815','20170817','20170818',
-                  '20170821','20170824','20170826','20170828','20170830',
-                  '20170831','20170902']
-dates2018_good = ['20180927','20180930','20181003','20181007','20181010',
-                  '20181012','20181015','20181017','20181019','20181021',
-                  '20181023']
-
-# Apply calibration to all data:    
-for date in dates2016_good: calibrate_file(date)
-for date in dates2017_good: calibrate_file(date)
-for date in dates2018_good: calibrate_file(date)
-
-print("===============================\n"
-      "!!! QC / Calibration complete !!!\n"
-      "==============================="
-      )
+        os.unlink(fname_calfile)    
 
 
 
+if __name__ == "__main__":
+    # Apply calibration to all data:    
+    for date in dates2016_good: calibrate_file(date)
+    for date in dates2017_good: calibrate_file(date)
+    for date in dates2018_good: calibrate_file(date)
 
-
-
+    print("===============================\n"
+        "!!! QC / Calibration complete !!!\n"
+        "==============================="
+        )
